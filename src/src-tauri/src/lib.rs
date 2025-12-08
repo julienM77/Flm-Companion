2d26dba6a17e1eb1b7a166c5e20867aba8bbc4c0
Julien M <4747563+julienM77@users.noreply.github.com>
2025-12-08 19:24:50 +0100

    update companion from setup

--- src/src-tauri/src/lib.rs
+++ src/src-tauri/src/lib.rs
@@ -23,6 +23,7 @@ pub fn run() {
             Some(vec![]),
         ))
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_dialog::init())
         .setup(|app| {
